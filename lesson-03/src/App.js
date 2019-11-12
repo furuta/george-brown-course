5549536767f25216d864764497e828ee31310474
amsul <reach@amsul.ca>
2019-11-12 17:57:42 -0500

    Add Lesson 3 files
    Signed-off-by: amsul <reach@amsul.ca>

---
+++ lesson-03/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className='App-content'>
+        <h1>Registration Form</h1>
+        {/*=== Your content goes here 👇 ===*/}
+      </div>
+    </div>
+  )
+}
