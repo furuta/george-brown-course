d1398516c7981543798f62a7aafd6527d6b95ea1
amsul <reach@amsul.ca>
2019-11-19 18:14:27 -0500

    Add Lesson 5 files
    Signed-off-by: amsul <reach@amsul.ca>

---
+++ lesson-05/src/App.js
@@ -0,0 +1,30 @@
+import './App.css'
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className='App-Content'>
+        <h1 className='App-Title'>Feedback Form</h1>
+
+        <h3 className='App-SubTitle'>Give a rating based on your experience</h3>
+        <Exercise1 />
+
+        <h3 className='App-SubTitle'>
+          Select all options based on your experience
+        </h3>
+        <Exercise2 />
+      </div>
+    </div>
+  )
+}
+
+function Exercise1() {
+  // Put your answers here for Exercise 1 👇
+  return null
+}
+
+function Exercise2() {
+  // Put your answers here for Exercise 2 👇
+  return null
+}
