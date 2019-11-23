24da790282a1b744b16518f679a3f35422171e10
furuta <>
2019-11-23 14:23:29 -0500

    delete config

--- project-01/src/firestoreConfig.js
+++ project-01/src/firestoreConfig.js
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyACk8mAACK-1LXtHJdHBf2hpNwLrO1Bzuk',
-  authDomain: 'fs3-checkout-form.firebaseapp.com',
-  databaseURL: 'https://fs3-checkout-form.firebaseio.com',
-  projectId: 'fs3-checkout-form',
-  storageBucket: 'fs3-checkout-form.appspot.com',
-  messagingSenderId: '834202142239',
-  appId: '1:834202142239:web:1205a000d5ae2f63f9b72a',
-}
+export const firebaseConfig = {}
